3f64884cc37f668441acd4257fb3bdaac4d7b252
rashidiy <bubbless7456@gmail.com>
2026-03-21 19:45:22 +0000

    Grant bot can_change_info permission for group photo setup

--- source/utils/services/pyrogram_service.py
+++ source/utils/services/pyrogram_service.py
@@ -199,7 +199,7 @@ async def setup_bot_admin(client, chat_id: int, bot_username: str) -> None:
             can_manage_video_chats=False,
             can_restrict_members=False,
             can_promote_members=False,
-            can_change_info=False,
+            can_change_info=True,
             can_invite_users=True,
             can_pin_messages=True,
             can_manage_topics=True,
